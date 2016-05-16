d959eea4eb8c18dfc3191e63650d7aae532f8a42
Hallvord R. M. Steen <hsteen@mozilla.com>
2016-05-16 14:23:08 +0200

    adding miscellaneous debug code

--- bootstrap.js
+++ bootstrap.js
@@ -2,6 +2,15 @@ const { classes: Cc, interfaces: Ci, utils: Cu } = Components;
 
 Cu.import("resource://gre/modules/Services.jsm");
 
+var prefs = {
+  debugVideoSrc:    false,
+  shimWindowEvent:  true,
+  logEvents:        ['click'],
+  // Note: log overrides ignore if event is listed in both
+  ignoreEvents:     ['unload', 'beforeunload']
+};
+
+
 var events = {};
 
 var eventShimObserver = {
@@ -30,24 +39,68 @@ function eventShimmer(e){
   }
 }
 
-function addListeners(win) {
-  for(var prop in win) {
-    if(/^on/.test(prop)) {
-      var evt = prop.replace(/^on/, '');
-      events[evt] = 1;
-      //Services.console.logStringMessage('Adding capturing listener for ' + evt + ' to ' + win);
-      win.addEventListener(evt, eventShimmer, true);
+function injectDebugJS(win) {
+  console.log(win.location);
+  if(prefs.shimWindowEvent) {
+    for(var prop in win) {
+      if(/^on/.test(prop)) {
+        var evt = prop.replace(/^on/, '');
+        events[evt] = 1;
+        //Services.console.logStringMessage('Adding capturing listener for ' + evt + ' to ' + win);
+        win.addEventListener(evt, eventShimmer, true);
+      }
     }
+  }
+  if(prefs.debugVideoSrc) {
+    // log scripts that set video.src (to debug for example 1228971)
+    var originalSetter = win.wrappedJSObject.document.createElement('video').__lookupSetter__('src');
+    win.wrappedJSObject.HTMLVideoElement.prototype.__defineSetter__('src', function(str){
+      console.log('Setting video src to ' + str);
+      if(str === null || str === '') {
+        try{undefined();}catch(e){console.log(e.stack);}
+      }
+      return originalSetter.call(this, str);
+    });
+  }
+
+  if(prefs.ignoreEvents.length || prefs.logEvents.length) {
+    // control what event listeners are added or log when they fire
+    // This isn't watertight - not modifying or tracking inline event handlers
+    var ael = win.wrappedJSObject.addEventListener;
+    win.wrappedJSObject.addEventListener = /* on window */
+      win.wrappedJSObject.Element.prototype.addEventListener = /* on elements */
+      win.wrappedJSObject.document.addEventListener = /* on document */
+      /* more objects needed? Probably.. */
+      function(evt, func, capture) {
+        var debugme;
+        if(evt === 'click' && this.tagName === 'DIV' && func.toString().indexOf('&&(!a||f.event.triggered!')>-1) {
+          // try{undefined();}catch(e){console.log( this + ' ' + this.tagName + ' ' + '\n' + e.stack.substr(-550))}
+          debugme = true;
+        }
+        if(prefs.logEvents.indexOf(evt) > -1 || debugme) {
+          ael.call(this, evt, (function(func){
+            return function(e) {
+              console.log('will now fire ' + e.type + ' on ' + e.target + ', listener ' + func);
+              debugger;
+              return func.call(this, e);
+            };
+          })(func), capture);
+        } else if(prefs.ignoreEvents.indexOf(evt) === -1) {
+          return ael.call(this, evt, func, capture);
+        } else {
+          console.log('Not adding listener for ' + evt + ' on ' + this);
+        }
+      };
   }
 }
 
 function loadIntoWindow(window) {
   if(window.BrowserApp) {
     window.BrowserApp.tabs.forEach(function(tab){
-      addListeners(tab.window);
+      injectDebugJS(tab.window);
     });
   } else {
-    addListeners(window);
+    injectDebugJS(window);
   }
 }
 
@@ -55,7 +108,7 @@ function unloadFromWindow(window) {
   window.BrowserApp.tabs.forEach(function(tab){
     for(var evt in events) {
       tab.window.removeEventListener(evt, eventShimmer, true);
-    };
+    }
   });
 }
 
